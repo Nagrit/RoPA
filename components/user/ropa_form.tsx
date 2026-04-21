722d4c4c0ff99861620ae6c265481b11be0ba84e
Thakdanai Ruksrithong <thakdanai.pk415@gmail.com>
2026-04-21 18:35:37 +0700

    Delete ropa_form.tsx

--- components/user/ropa_form.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export function RopaForm() {
-  return (
-    <div className="min-h-screen bg-[#091833] flex flex-col items-center justify-center text-white p-4">
-      <p>Hi this is ropa form</p>
-    </div>
-  );
-}
